9152e7e78fbc39de454b418706ddee3319912f43
diego.salazar01 <diego.salazar01@globant.com>
2025-09-05 18:20:14 -0500

    Add LogoutTest to validate redirect to login page

--- src/test/java/com/automationProject/pages/HomePage.java
+++ src/test/java/com/automationProject/pages/HomePage.java
@@ -3,6 +3,9 @@ package com.automationProject.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.Random;
@@ -12,6 +15,8 @@ public class HomePage {
 
     private By addToCartButton = By.className("btn_inventory");
     private By cartButton = By.className("shopping_cart_link");
+    private By sidebarButton = By.id("react-burger-menu-btn");
+    private By logoutButton = By.id("logout_sidebar_link");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
@@ -38,5 +43,17 @@ public class HomePage {
     public void navigateToCartPage() {
         WebElement navigateButton = driver.findElement(cartButton);
         navigateButton.click();
+    }
+
+    public void clickSidebarButton() {
+        WebElement sidebar = driver.findElement(sidebarButton);
+        sidebar.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(logoutButton));
+    }
+    public void clickLogoutButton() {
+        WebElement logout = driver.findElement(logoutButton);
+        logout.click();
     }
 }
